f702033693236b632a50b0969ec1968d86029fc6
banderop123 <com32149@gmail.com>
2021-01-17 14:59:35 +0300

    more cras

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const nissan_cars = document.getElementById("nissan_cars");
 let toyota_cars = document.getElementById("toyota_cars");
 const boxing = document.getElementById("package");
 const kia_cars = document.getElementById("kia_cars");
-const hyundai_cars = document.getElementById("hyundai_cars")
+const hyundai_cars = document.getElementById("hyundai_cars");
+const chevrolet_cars = document.getElementById("chevrolet_cars");
+const ford_cars = document.getElementById("ford_cars");
+const GMC_cars = document.getElementById("GMC_cars");
 
 function hide() {
     nissan_cars.style.display = "none";
@@ -12,8 +15,26 @@ function hide() {
     boxing.style.display = "none";
     kia_cars.style.display = "none";
     hyundai_cars.style.display = "none";
+    chevrolet_cars.style.display = "none";
+    ford_cars.style.display = "none";
+    GMC_cars.style.display = "none";
+
 }
 
+function GMC() {
+    hide();
+    GMC_cars.style.display = "block";
+}
+
+function ford() {
+    hide();
+    ford_cars.style.display = "block";
+}
+
+function chevrolet() {
+    hide();
+chevrolet_cars.style.display = "block";
+}
 
 function nissan(){
     hide();
